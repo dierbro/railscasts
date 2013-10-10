63761b5fa60d7d6d813b6b4959f662413ce5bb4b
Diego R. Brogna <dierbro@gmail.com>
2013-10-10 10:23:32 +0200

    Add secret_token initializer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railscasts::Application.config.secret_token = APP_CONFIG['session_secret']
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railscasts::Application.config.secret_key_base = 'f594f4cf597c5f18b4fb66955edea94eded2ca6f669b612a0bf787cca0036771a2c03fec969a9942c34ff056c317d144083943b4f90218ab13ec6dfcf897510e'
